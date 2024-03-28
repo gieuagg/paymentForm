c55a7c2668eecc7489002457a9b4723ccf287980
evagg <evagggianna@gmail.com>
2024-03-28 20:20:14 +0200

    Added conversion pipe for distance units, ccurrency and date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'paymentForm';
+  name: string ="";
+  date: string ="";
+  amount: number = 0;
+  height: number = 0;
+  miles: number = 0;
+
+  car = {
+    make: 'Toyota',
+  model: 'Camry',
+  year: 2000
+  }
+  onMilesChange(value: string){
+    this.miles = parseFloat(value);
+  }
+  onHeightChange(value: string){
+    this.height = parseFloat(value);
+  }
+  onNameChange(value: string){
+    this.name = value;
+  }
+  onDateChange(value: string){
+    this.date = value;
+  }
+  onAmountChange(value: string){
+    this.amount = parseFloat(value);
+  }
 }
